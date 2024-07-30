c28de904d2400827351b4a3e83b919ee422d213e
zhaoyafei <zhaoyafei0210@gmail.com>
2024-07-31 02:41:07 +0300

    add run_python_example*.sh

---
+++ run_python_example_basic_room.sh
@@ -0,0 +1,8 @@
+# LIVEKIT_API_KEY=devkey LIVEKIT_API_SECRET=secret \
+#     python generate_token.py
+
+export LIVEKIT_URL="http://127.0.0.1:7880"
+export LIVEKIT_API_KEY=devkey
+export LIVEKIT_API_SECRET=secret
+
+python ../python-sdks/examples/basic_room.py

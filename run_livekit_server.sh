8d507280e16d54a25f5fc1475bab5e3d33c5c735
zhaoyafei <zhaoyafei0210@gmail.com>
2024-07-31 02:40:36 +0300

    add run_livekit_server.sh

--- run_livekit_server.sh
+++ run_livekit_server.sh
@@ -1,6 +1,2 @@
-export LIVEKIT_URL=127.0.0.1
-export LIVEKIT_API_KEY=devkey
-export LIVEKIT_API_SECRET=secret
-
 livekit-server --dev --bind 127.0.0.1
 # livekit-server --dev --bind 192.168.0.92 --bind 127.0.0.1
